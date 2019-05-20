447c518ab8799bda7533c404676eb3f2a6d8d6d5
nicolas <nicolachoquet06250@gmail.com>
2019-05-20 13:23:39 +0200

    add pwa elements

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
+const CACHE_NAME = 'V1';
+const STATIC_CACHE_URLS = ['/', 'style.css', 'script.js'];
+
 self.addEventListener('install', event => {
 	console.log('Service Worker installing.');
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_CACHE_URLS))
+	)
 });
 
 self.addEventListener('activate', event => {
