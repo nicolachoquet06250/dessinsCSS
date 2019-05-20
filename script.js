a4d29c9696c712cb8d080b923ba6c519d15dbae4
nicolas <nicolachoquet06250@gmail.com>
2019-05-20 13:59:44 +0200

    add pwa elements

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ function on_submit() {
 	}
 }
 
+function installApp() {
+	// Show the prompt
+	deferredPrompt.prompt();
+	setupButton.disabled = true;
+	// Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+				console.log('PWA setup accepted');
+				// hide our user interface that shows our A2HS button
+				setupButton.style.display = 'none';
+			} else {
+				console.log('PWA setup rejected');
+			}
+			deferredPrompt = null;
+		});
+}
+
 window.onload = () => {
 	function get_hours() {
 		let date = new Date();
@@ -18,6 +36,27 @@ window.onload = () => {
 
 	get_hours();
 	setInterval(get_hours, 1000);
+
+	let deferredPrompt; // Allows to show the install prompt
+	let setupButton;
+
+	window.addEventListener('beforeinstallprompt', e => {
+		// Prevent Chrome 67 and earlier from automatically showing the prompt
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
+		console.log("beforeinstallprompt fired");
+		if (setupButton == undefined) {
+			setupButton = document.getElementById("setup_button");
+		}
+		// Show the setup button
+		setupButton.style.display = "inline";
+		setupButton.disabled = false;
+	});
+
+	window.addEventListener('appinstalled', e => {
+		console.log("appinstalled fired", e);
+	});
 };
 
 if ('serviceWorker' in navigator) {
